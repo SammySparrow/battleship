98b3c56568072c747941e2e38cdc0ba11168e933
SammySparrow <217705499+SammySparrow@users.noreply.github.com>
2026-01-05 14:37:58 +1100

    Write more tests for ship placement
    Implemented two more tests for ship placement method,
    aiming to test if the method will throw an error if
    an illegal move is made.

--- tests/game.test.js
+++ tests/game.test.js
@@ -41,16 +41,45 @@ describe("Gameboard tests", () => {
     gb = new Gameboard();
   });
 
-  test("Ship exists in determined coordinates", () => {
-    let threeTileShip = new Ship(3);
+  describe("Ship placement method tests", () => {
+    test("Ship exists in determined coordinates", () => {
+      let threeTileShip = new Ship(3);
 
-    gb.placeShip(threeTileShip, [
-      [2, 3],
-      [2, 4],
-      [2, 5],
-    ]);
-    expect(gb.board[2][3]).toBe(threeTileShip);
-    expect(gb.board[2][4]).toBe(threeTileShip);
-    expect(gb.board[2][5]).toBe(threeTileShip);
+      gb.placeShip(threeTileShip, [
+        [2, 3],
+        [2, 4],
+        [2, 5],
+      ]);
+      expect(gb.board[2][3]).toBe(threeTileShip);
+      expect(gb.board[2][4]).toBe(threeTileShip);
+      expect(gb.board[2][5]).toBe(threeTileShip);
+    });
+
+    test("Ship cannot be placed outside of gameboard boundaries", () => {
+      let twoTileShip = new Ship(2);
+
+      expect(() =>
+        gb.placeShip(twoTileShip, [
+          [9, 9],
+          [9, 10],
+        ])
+      ).toThrow(Error);
+    });
+
+    test("Ships cannot overlap", () => {
+      let twoTileShip = new Ship(2);
+      let secondTwoTileShip = new Ship(2);
+
+      gb.placeShip(twoTileShip, [
+        [3, 4],
+        [3, 5],
+      ]);
+      expect(() =>
+        gb.placeShip(secondTwoTileShip, [
+          [4, 5],
+          [3, 5],
+        ])
+      ).toThrow(Error);
+    });
   });
 });
