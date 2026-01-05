7447229c8058902308cd0cc031f89cdb10ed19de
SammySparrow <217705499+SammySparrow@users.noreply.github.com>
2026-01-05 11:09:10 +1100

    Write additional ship class tests

--- tests/game.test.js
+++ tests/game.test.js
@@ -10,4 +10,20 @@ describe("Ship tests", () => {
   test("Ship initialises with expected length", () => {
     expect(ship.length).toBe(3);
   });
+
+  test("timesHit property initialises to 0", () => {
+    expect(ship.timesHit).toBe(0);
+  });
+
+  test("timesHit increments whe ship is hit", () => {
+    ship.hit;
+    expect(ship.timesHit).toBe(1);
+  });
+
+  test("isSunk status is true when timesHit equals ship length", () => {
+    ship.hit;
+    ship.hit;
+    ship.hit;
+    expect(ship.sunk).toBe(true);
+  });
 });
