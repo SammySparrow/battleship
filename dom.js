8da6445f0f6d8fbcad3794270cdefeee83de0391
SammySparrow <217705499+SammySparrow@users.noreply.github.com>
2026-01-14 08:38:58 +1100

    Create cell class

--- dom.js
+++ dom.js
@@ -3,14 +3,17 @@ export { render };
 function render(parent, player) {
   const main = document.querySelector(`${parent}`);
   const gridHolder = document.createElement("div");
+  gridHolder.setAttribute("class", "wrapper");
 
   for (let i = 0; i < 10; i++) {
     for (let j = 0; j < 10; j++) {
       let cell = document.createElement("div");
       cell.setAttribute("class", "cell");
-      if (player.board.grid[i][j] === undefined) {
+      cell.setAttribute("data-x", `${i}`);
+      cell.setAttribute("data-y", `${j}`);
+      if (player.board.grid[j][i] === undefined) {
         cell.style.backgroundColor = "white";
-      } else if (player.board.grid[i][j] === "miss") {
+      } else if (player.board.grid[j][i] === "miss") {
         cell.style.backgroundColor = "red";
       } else {
         cell.style.backgroundColor = "grey";
--- index.js
+++ index.js
@@ -8,6 +8,8 @@ playerOne.board.placeShip(4, [0, 0], "vertical");
 playerOne.board.placeShip(5, [1, 5], "horizontal");
 playerOne.board.placeShip(4, [6, 9], "horizontal");
 playerOne.board.placeShip(3, [6, 6], "vertical");
+playerTwo.board.placeShip(4, [0, 0], "vertical");
+playerTwo.board.receiveAttack([7, 1]);
 
-console.log(playerOne.board.grid[0][0]);
 render("main", playerOne);
+render("main", playerTwo);
