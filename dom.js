300b6c4ac2c82cd065f876a440eb1ddcd6823e2d
SammySparrow <217705499+SammySparrow@users.noreply.github.com>
2026-01-13 15:19:38 +1100

    Implement first version of render function
    Implemented the first working version of a rendering
    function to print out the board, added a style sheet
    to lessen the amount of code needed for styling within
    the DOM module.

--- dom.js
+++ dom.js
@@ -2,5 +2,22 @@ export { render };
 
 function render(parent, player) {
   const main = document.querySelector(`${parent}`);
-  main.textContent = "aloo";
+  const gridHolder = document.createElement("div");
+
+  for (let i = 0; i < 10; i++) {
+    for (let j = 0; j < 10; j++) {
+      let cell = document.createElement("div");
+      cell.setAttribute("class", "cell");
+      if (player.board.grid[i][j] === undefined) {
+        cell.style.backgroundColor = "white";
+      } else if (player.board.grid[i][j] === "miss") {
+        cell.style.backgroundColor = "red";
+      } else {
+        cell.style.backgroundColor = "grey";
+      }
+      gridHolder.appendChild(cell);
+    }
+  }
+
+  main.appendChild(gridHolder);
 }
--- index.js
+++ index.js
@@ -9,4 +9,5 @@ playerOne.board.placeShip(5, [1, 5], "horizontal");
 playerOne.board.placeShip(4, [6, 9], "horizontal");
 playerOne.board.placeShip(3, [6, 6], "vertical");
 
+console.log(playerOne.board.grid[0][0]);
 render("main", playerOne);
